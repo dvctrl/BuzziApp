3175c1652690e50fea22b1ba6c834a0731ac0267
dvctrl <davidmouzinho.contato@gmail.com>
2023-02-16 22:06:02 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/buzzi/login_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.buzzi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
